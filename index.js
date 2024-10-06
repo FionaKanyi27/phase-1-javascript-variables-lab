b577c1aebea961bf04568b3637e1a4bb501604f3
Fiona Kanyi <fionakanyi0@gmail.com>
2024-10-06 23:34:10 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+// Define the companyName variable and set it as a constant with the value 'Scuber'
+const companyName = 'Scuber';
+
+// Define mostProfitableNeighborhood with an appropriate value
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Define companyCeo with a name
+let companyCeo = 'Susan Smith';
